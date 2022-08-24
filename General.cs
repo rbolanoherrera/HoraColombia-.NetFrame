7bb1798e17bc06c59bc5f375e5e5cde22d995782
Rafael Bolaños Herrera <rbolanoherrera@gmail.com>
2022-08-24 11:25:20 -0500

    Create General.cs
    Obtener la zona horaria de Bogota Colobia

---
+++ General.cs
@@ -0,0 +1,19 @@
+public static class FuncionesGenerales
+{
+    /// <summary>
+    /// Hora de Colombia tomando como ciudad Bogota
+    /// </summary>
+    /// <returns></returns>
+    public static DateTime HoraColombia()
+    {
+        //Cambiar formato de fecha a la hora de Colombia
+        DateTime localNow = DateTime.Now;
+
+        ReadOnlyCollection<TimeZoneInfo> timeZoneInfoCollectio = TimeZoneInfo.GetSystemTimeZones();
+        TimeZoneInfo colDate = TimeZoneInfo.FindSystemTimeZoneById("SA Pacific Standard Time");//(UTC-05:00) Bogotá, Lima, Quito, Rio Branco");
+
+        DateTime fechaCol = TimeZoneInfo.ConvertTimeFromUtc(localNow.ToUniversalTime(), colDate);
+
+        return fechaCol;
+    }
+}
